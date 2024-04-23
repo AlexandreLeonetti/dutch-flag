c47be6dfe5b78e6419f2e537bbf1c4dbcac43f45
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-23 20:01:34 +0200

    dutch-flag

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+/* dutch flag algo */
+const test = [2, 1, 2, 0, 2, 1, 1, 1, 2, 0, 0, 2, 2, 1, 0];
+export function dutch(A, p) {
+    /* inferior to pivot, permutations */
+    for (let i = 0; i < (A.length - 1); i++) {
+        for (let j = i + 1; j <= (A.length - 1); j++) {
+            if (A[j] < p) {
+                [A[i], A[j]] = [A[j], A[i]];
+                break;
+            }
+        }
+    }
+    /* superior to pivot */
+    for (let i = (A.length - 1); i > 0; i--) {
+        for (let j = 0; j < (i); j++) {
+            if (A[j] > p) {
+                [A[i], A[j]] = [A[j], A[i]];
+                break;
+            }
+        }
+    }
+    return A;
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# dutch flag sorting algorithm. 
+
+> Sort arrays according to dutch flag algorithm. 
+
+## Install
+
+```sh
+npm install @leonetti/dutch-flag
+```
+
+## Usage
+
+```js
+import {dutch} from '@leonetti/dutch-flag';
+
+dutch([2, 1, 2, 0, 2, 1, 1, 1, 2, 0, 0, 2, 2, 1, 0],1) // returns sorted array according to pivot 1.
+
+```
